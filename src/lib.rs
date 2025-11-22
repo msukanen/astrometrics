a42f25f468a84a4455b9870f260d7e815627df4e
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-22 18:36:26 +0200

    plug in 'Temperature'.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 // [Mass]
 mod mass;
-pub use mass::AsMass;
-pub use mass::Mass;
+pub use mass::{Mass, AsMass};
 // [Temperature]
 mod temperature;
+pub use temperature::{Temperature, AsTemperature};
 
+// Whenever 'f128' is stable, we're ready for it.
 #[cfg(not(feature = "f128_stable"))]
 type MetricsInternalType = f64;
 #[cfg(feature = "f128_stable")]
