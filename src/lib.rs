50e62f5f4d8717ce3351c073752ae71ea05f6f8e
Markku Sukanen <markku.sukanen@gmail.com>
2025-11-23 23:35:37 +0200

    revert the clownage...

--- src/lib.rs
+++ src/lib.rs
@@ -15,26 +15,24 @@ type MetricsInternalType = f64;
 type MetricsInternalType = f128;
 
 /// Define primitives-related ops for one or other metric.
-/// 
-/// Using `self.clone()` because e.g. RangeInclusive isn't Copy due some idiot in the stdlib team had a brainfart…
 #[macro_export]
 macro_rules! define_prim_ops_for_metric {
     (f [$( $bits:expr ),+]; $metric:ident) => {paste!{$(
         impl Add<[<f $bits>]> for $metric {
             type Output = Self;
-            fn add(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() + rhs as MetricsInternalType); s }
+            fn add(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() + rhs as MetricsInternalType); s }
         }
         impl Sub<[<f $bits>]> for $metric {
             type Output = Self;
-            fn sub(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() - rhs as MetricsInternalType); s }
+            fn sub(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() - rhs as MetricsInternalType); s }
         }
         impl Div<[<f $bits>]> for $metric {
             type Output = Self;
-            fn div(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() / rhs as MetricsInternalType); s }
+            fn div(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() / rhs as MetricsInternalType); s }
         }
         impl Mul<[<f $bits>]> for $metric {
             type Output = Self;
-            fn mul(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() * rhs as MetricsInternalType); s }
+            fn mul(self, rhs: [<f $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() * rhs as MetricsInternalType); s }
         }
         impl Mul<$metric> for [<f $bits>] {
             type Output = $metric;
@@ -45,19 +43,19 @@ macro_rules! define_prim_ops_for_metric {
         // Unsigned ones …
         impl Add<[<u $bits>]> for $metric {
             type Output = Self;
-            fn add(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() + rhs as MetricsInternalType); s }
+            fn add(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() + rhs as MetricsInternalType); s }
         }
         impl Sub<[<u $bits>]> for $metric {
             type Output = Self;
-            fn sub(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() - rhs as MetricsInternalType); s }
+            fn sub(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() - rhs as MetricsInternalType); s }
         }
         impl Div<[<u $bits>]> for $metric {
             type Output = Self;
-            fn div(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() / rhs as MetricsInternalType); s }
+            fn div(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() / rhs as MetricsInternalType); s }
         }
         impl Mul<[<u $bits>]> for $metric {
             type Output = Self;
-            fn mul(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() * rhs as MetricsInternalType); s }
+            fn mul(self, rhs: [<u $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() * rhs as MetricsInternalType); s }
         }
         impl Mul<$metric> for [<u $bits>] {
             type Output = $metric;
@@ -66,19 +64,19 @@ macro_rules! define_prim_ops_for_metric {
         // Signatures for all!
         impl Add<[<i $bits>]> for $metric {
             type Output = Self;
-            fn add(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() + rhs as MetricsInternalType); s }
+            fn add(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() + rhs as MetricsInternalType); s }
         }
         impl Sub<[<i $bits>]> for $metric {
             type Output = Self;
-            fn sub(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() - rhs as MetricsInternalType); s }
+            fn sub(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() - rhs as MetricsInternalType); s }
         }
         impl Div<[<i $bits>]> for $metric {
             type Output = Self;
-            fn div(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() / rhs as MetricsInternalType); s }
+            fn div(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() / rhs as MetricsInternalType); s }
         }
         impl Mul<[<i $bits>]> for $metric {
             type Output = Self;
-            fn mul(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self.clone(); s.set(self.raw() * rhs as MetricsInternalType); s }
+            fn mul(self, rhs: [<i $bits>]) -> Self::Output { let mut s = self; s.set(self.raw() * rhs as MetricsInternalType); s }
         }
         impl Mul<$metric> for [<i $bits>] {
             type Output = $metric;
@@ -94,7 +92,7 @@ macro_rules! define_ops_for_metric {
             // The root of all not-so-evil…
             impl $trait<&$metric> for &$metric {
                 type Output = $metric;
-                fn $fn(self, rhs: &$metric) -> Self::Output { self.clone().$fn(rhs.raw()) }
+                fn $fn(self, rhs: &$metric) -> Self::Output { (*self).$fn(rhs.raw()) }
             }
             impl $trait for $metric {
                 type Output = $metric;
