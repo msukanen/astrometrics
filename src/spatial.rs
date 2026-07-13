1de6e940a6a3bf399dc1c48459baabcc3bd40dd7
Markku Sukanen <markku.sukanen@gmail.com>
2026-07-14 00:04:56 +0300

    megastruct pub(crate).

--- src/spatial.rs
+++ src/spatial.rs
@@ -5,7 +5,7 @@ use paste::paste;
 use serde::{Deserialize, Serialize};
 
 pub mod iau;
-mod megastruct;
+pub(crate) mod megastruct;
 use crate::{DefoAble, MetricsInternalType, Squared, defo, iau::*, ratio};
 
 #[derive(Debug, Deserialize, Serialize, Clone, Copy)]
@@ -304,26 +304,5 @@ impl MulAssign<f64> for SpatialUnit {
 impl MulAssign<f32> for SpatialUnit {
     fn mul_assign(&mut self, rhs: f32) {
         self.mul_assign(rhs as f64);
-    }
-}
-
-#[cfg(test)]
-mod spatial_tests {
-    use super::*;
-    use crate::spatial::megastruct::{Megastructure, SpatialContained};
-
-    #[test]
-    fn gr_range_works() {
-        let gr = Megastructure::from(((6.ly(), 12.ly()), (15.ly(), 30.ly()), (40.ly(), 42.ly())));
-        assert_eq!(Some(SpatialContained::VisibleDisk), gr.contains(&7.0.ly()));
-        assert_ne!(Some(SpatialContained::Arms), gr.contains(&(15.0 - f64::EPSILON*10.0).ly()));
-        assert_eq!(Some(SpatialContained::Arms), gr.contains(&(15.0 + f64::EPSILON*10.0).ly()));
-    }
-
-    #[test]
-    fn comparison() {
-        let a = 1.ly();
-        assert!(a < 2);
-        assert!(2 > a);
     }
 }
